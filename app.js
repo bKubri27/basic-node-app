58aa2115667326ccc166064d35fb6acdd1f509ba
BelindaK <belindakubri@gmail.com>
2022-11-18 03:00:53 -0600

    modifed ports

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ app.get('/',(req, res) => {
     res.send("welcome to the home page sweetheart");
 });
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("Wazzapp Queen B");
 });
 
